9cc0e7c2c90cd3c7e29de6f63edbcbc0febfd9bd
hanchuan <>
2021-01-27 01:24:10 +0100

    rearrange and improve the script

--- bin/util/extractmodulepart.py
+++ bin/util/extractmodulepart.py
@@ -11,6 +11,11 @@ It was originally written to solve the problem that the bash shell script could 
 So the whole design philosophy is to maximize the portability, i.e. write in a cross-platform style.
 """
 
+########################################################################################
+####################                    FUNCTIONS                   ####################
+########################################################################################
+
+# function to show the help message
 def show_helpmesseage():
     print("\n""USAGE: "+os.path.basename(__file__)+" module_dir FOLDER_1 [FOLDER_2 ...]\n\n"
           "module_dir is the folder containing the DUNE module from which you\n"
@@ -20,105 +25,9 @@ def show_helpmesseage():
           "you would like to timber a table in dumux-pub.")
     exit(1)
 
-# check if help is needed
-if (len(sys.argv) < 2):
-    show_helpmesseage()
-if (str(sys.argv[1]) == "--help" or
-    str(sys.argv[1]) == "-help" or
-    str(sys.argv[1]) == "help"):
-    show_helpmesseage()
-module_dir = str(sys.argv[1])
-
-# if module_dir contains a slash as last character, delete it
-if module_dir.endswith('/'):
-   module_dir = module_dir[:-1]
-
-# check if we are above module_dir
-if not (os.path.isdir(module_dir)):
-    print("ERROR: you need to run the script one level above the folder "+module_dir+".")
-    print("Run \""+os.path.basename(__file__)+" --help\" for details.")
-    exit(1)
-
-# determine all source files in the paths passed as arguments
-os.chdir(module_dir)
-module_full_path=os.getcwd()
-all_sources=[]
-all_sources_with_path = []
-all_directories=[]
-for dir_path in sys.argv[2:]:
-    stripped_path = dir_path.removeprefix(module_dir)
-    directories = " " + stripped_path
-    all_directories.append(stripped_path)
-    os.chdir( os.path.join(module_full_path + stripped_path))
-    for file in glob.glob("*.cc"):
-        sources = os.path.join(stripped_path, file)
-        all_sources.append(sources)
-        sourceswithpath = os.path.join(module_full_path + sources)
-        all_sources_with_path.append(sourceswithpath)
-os.chdir(module_full_path)
-os.chdir("..") # back to the script folder
-
-# check if sources have been obtained
-if (all_sources == []):
-    print("ERROR: no source files *.cc found in the directories "+sys.argv[2]+".")
-    print("Be sure to provide a list of paths as arguments to this script.")
-    print("Run \""+os.path.basename(__file__)+" --help\" for details.")
-    exit(1)
-
-# try to find the duneproject script
-dune_project = shutil.which('duneproject', path = "dune-common/bin")
-if (dune_project == None):
-    print("ERROR: Could not find duneproject.")
-    print("Be sure to either have duneproject in your search path")
-    print("or to run this script from a directory that contains duneproject.")
-    exit(1)
-else:
-    print(dune_project)
-
-# give explanations
-print("\n""This script will\n"
-      "- extract the following sub-folders of "+ module_dir +":\n\n")
-for dir_path in all_directories:
-    print("  "+ dir_path + ",")
-print("\n""  and all headers in "+ module_dir + " that are required to build the\n"
-      "  executables from the sources\n\n")
-for source in all_sources:
-    print("  "+ source +",")
-print("\n- copy the extracted files into a freshly created DUNE module, retaining the\n"
-      "  directory structure,\n\n"
-      "- update/create all required files like CMakeLists.txt,\n\n"
-      "- store the versions of all used Dune module\n\n"
-      "- and extract their modifications as patches.\n\n"
-      "Thus, you receive a fully-working DUNE module containing the subset of\n"
-       + module_dir + " that is required to run your application.\n"
-      "duneproject will be run now. The new module should NOT depend on the\n"
-      "module in "+ module_dir + ".\n\n"
-      "Read the above and press [Enter] to proceed...",end="")
-
-# run duneproject
-old_ls = os.listdir()
-subprocess.call([dune_project], shell=True)
-new_ls  = os.listdir()
-
-# determine the new module/directory name
-module_name = (set(new_ls) - set(old_ls)).pop()
-if (module_name  == ""):
-    print("ERROR: could not find new module. Aborting.")
-    exit(1)
-else:
-    print()
-    print(os.path.basename(__file__) + ": Found new module " + module_name)
-print("Determining required headers...")
-
-os.chdir(module_name)
-module_path=os.getcwd()
-
-# extract all headers
+# function to search the header file efficiently with parallel programming
 import re
 import threading
-
-# define the function to search all header files
-all_headers = []
 def search_headers(c_file):
     global all_headers
     f = open(c_file,'r')
@@ -143,27 +52,24 @@ def search_headers(c_file):
             thread_ = threading.Thread(target = search_headers, args = (header_with_path, ))
             thread_.start()
 
-# provide some output and copy everything to the new module
-for source in all_sources_with_path:
-    search_headers(source)
-    for header in all_headers:
-        print(header)
-        dir_path = os.path.dirname(os.path.realpath(header)).replace(module_dir,module_name,1)
-        os.makedirs(dir_path, exist_ok=True)
-        shutil.copy(header,dir_path)
-    source_dir = os.path.dirname(source)
-    source_path = source_dir.replace(module_dir,module_name,1)
-    copy_tree(source_dir, source_path)
+# functions to find the file with specific pattern or name in a direcotry
+def find_all(name, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            result.append(os.path.join(root, name))
+    return result
 
-# delete all architecture-dependent files and unneeded directories
-rmfilenames = ["Makefile.in", "Makefile", '*.o', '*.deps/*']
-for rmfile in rmfilenames:
-    for filename in glob.glob(rmfile):
-        os.remove(filename)
-shutil.rmtree('dune')
-shutil.rmtree('src')
+import fnmatch
+def find(pattern, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, pattern):
+                result.append(os.path.join(root, name))
+    return result
 
-# set CMake File for each directory
+# functions to configure CMakeLists.txt files
 def __generate_new_content(lines, pattern, replace_str):
     index = lines.find(pattern)
     if index != -1:
@@ -244,7 +150,6 @@ def __check_dir(root_dir):
 
 def __check_str(root_dir):
     if root_dir is None or root_dir.strip() == "":
-        print("root path is None!")
         return None
     root_dir = root_dir.strip()
     if root_dir.endswith(os.sep):
@@ -268,7 +173,153 @@ def generate_cmake_lists_txt_file(root_dir):
                     header_files.append(name)
         __generate_cmake_lists_txt(cmake_list_txt_file, dirs, header_files, destination)
 
+
+########################################################################################
+####################                    PROCESS                     ####################
+########################################################################################
+
+# check if help is needed
+if (len(sys.argv) < 2):
+    show_helpmesseage()
+if (str(sys.argv[1]) == "--help" or
+    str(sys.argv[1]) == "-help" or
+    str(sys.argv[1]) == "help"):
+    show_helpmesseage()
+module_dir = str(sys.argv[1])
+
+# if module_dir contains a slash as last character, delete it
+if module_dir.endswith('/'):
+   module_dir = module_dir[:-1]
+
+# check if we are above module_dir
+if not (os.path.isdir(module_dir)):
+    print("ERROR: you need to run the script one level above the folder "+module_dir+".")
+    print("Run \""+os.path.basename(__file__)+" --help\" for details.")
+    exit(1)
+
+# determine all source files in the paths passed as arguments
+os.chdir(module_dir)
+module_full_path=os.getcwd()
+all_sources=[]
+all_sources_with_path = []
+all_directories=[]
+for dir_path in sys.argv[2:]:
+    stripped_path = dir_path.removeprefix(module_dir)
+    directories = " " + stripped_path
+    all_directories.append(stripped_path)
+    os.chdir(os.path.join(module_full_path + stripped_path))
+    for file in find("*.cc", os.path.join(module_full_path + stripped_path)):
+        sources = os.path.join(stripped_path, file).removeprefix(module_full_path)
+        all_sources.append(sources)
+        sourceswithpath = os.path.join(module_full_path + sources)
+        all_sources_with_path.append(sourceswithpath)
+os.chdir(module_full_path)
+os.chdir("..") # back to the script folder
+
+# check if sources have been obtained
+if (all_sources == []):
+    print("ERROR: no source files *.cc found in the directories "+sys.argv[2]+".")
+    print("Be sure to provide a list of paths as arguments to this script.")
+    print("Run \""+os.path.basename(__file__)+" --help\" for details.")
+    exit(1)
+
+# try to find the duneproject script
+dune_project = shutil.which('duneproject', path = "dune-common/bin")
+if (dune_project == None):
+    print("ERROR: Could not find duneproject.")
+    print("Be sure to either have duneproject in your search path")
+    print("or to run this script from a directory that contains duneproject.")
+    exit(1)
+else:
+    print(dune_project)
+
+# give explanations
+print("\n""This script will\n"
+      "- extract the following sub-folders of "+ module_dir +":\n\n")
+for dir_path in all_directories:
+    print("  "+ dir_path + ",")
+print("\n""  and all headers in "+ module_dir + " that are required to build the\n"
+      "  executables from the sources\n\n")
+for source in all_sources:
+    print("  "+ source +",")
+print("\n- copy the extracted files into a freshly created DUNE module, retaining the\n"
+      "  directory structure,\n\n"
+      "- update/create all required files like CMakeLists.txt,\n\n"
+      "- store the versions of all used Dune module\n\n"
+      "- and extract their modifications as patches.\n\n"
+      "Thus, you receive a fully-working DUNE module containing the subset of\n"
+       + module_dir + " that is required to run your application.\n"
+      "duneproject will be run now. The new module should NOT depend on the\n"
+      "module in "+ module_dir + ".\n\n")
+
+input("Read the above and press [Enter] to proceed...")
+
+# run duneproject
+old_ls = os.listdir()
+subprocess.call([dune_project], shell=True)
+new_ls  = os.listdir()
+
+# determine the new module/directory name
+module_name = (set(new_ls) - set(old_ls)).pop()
+if (module_name  == ""):
+    print("ERROR: could not find new module. Aborting.")
+    exit(1)
+else:
+    print()
+    print(os.path.basename(__file__) + ": Found new module " + module_name)
+print("Determining required headers...")
+os.chdir(module_name)
+module_path=os.getcwd()
+
+# extract all headers, provide some output and copy everything to the new module
+all_headers = []
+print("The following header files are extracted: ")
+for source in all_sources_with_path:
+    search_headers(source)
+    for header in all_headers:
+        print(header)
+        dir_path = os.path.dirname(os.path.realpath(header)).replace(module_dir,module_name,1)
+        os.makedirs(dir_path, exist_ok=True)
+        shutil.copy(header,dir_path)
+    source_dir = os.path.dirname(source)
+    source_path = source_dir.replace(module_dir,module_name,1)
+    copy_tree(source_dir, source_path)
+
+# delete all architecture-dependent files and unneeded directories
+rmfilenames = ["Makefile.in", "Makefile", '*.o', '*.deps/*']
+for rmfile in rmfilenames:
+    for filename in glob.glob(rmfile):
+        os.remove(filename)
+shutil.rmtree('dune')
+shutil.rmtree('src')
+
+# set CMakeLists.txt for each directory
 generate_cmake_lists_txt_file(module_path)
+
+# add includes to the automatically generated file *Macros.cmake
+os.chdir("..") # back to the script folder
+macro_file = find('*Macros.cmake', module_name)
+macros_camke = open(macro_file[0], "a")
+cmake_files = find_all("CMakeLists.txt", module_name)
+module_dir = os.path.join(module_name,"cmake/modules")
+for cmake_file in cmake_files:
+    with open(cmake_file) as cmakefile:
+        if 'add_csv_file_links' in cmakefile.read():
+            shutil.copy("dumux-devel/cmake/modules/AddCSVFileLinks.cmake", module_dir)
+            macros_camke.write("\ninclude(AddCSVFileLinks)")
+        if 'add_executable_all' in cmakefile.read():
+            shutil.copy("dumux-devel/cmake/modules/AddExecutableAll.cmake", module_dir)
+            macros_camke.write("\ninclude(AddExecutableAll)")
+        if 'add_file_link' in cmakefile.read():
+            shutil.copy("dumux-devel/cmake/modules/AddFileLink.cmake", module_dir)
+            macros_camke.write("\ninclude(AddFileLink)")
+        if 'add_folder_link' in cmakefile.read():
+            shutil.copy("dumux-devel/cmake/modules/AddFolderLink.cmake", module_dir)
+            macros_camke.write("\ninclude(AddFolderLink)")
+        if 'add_gnuplot_file_links' in cmakefile.read():
+            shutil.copy("dumux-devel/cmake/modules/AddGnuplotFileLinks.cmake", module_dir)
+            macros_camke.write("\ninclude(AddGnuplotFileLinks)")
+macros_camke.close()
 
 # move patches folder into module if existing
 if (os.path.isdir("patches") ):
